ca3bc13691276c8f5e91cd12d2f3b94e81001da8
Victor Khomyakov <homyakov.vs@edu.spbstu.ru>
2022-11-26 23:53:21 +0300

    Vigenere cypher implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from string import ascii_lowercase as alphabet
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -17,3 +18,19 @@ async def get_caesar_cypher(type, plaintext, shift=1):
 async def get_caesar_cypher(type, plaintext):
 	if type in ['encrypt', 'decrypt']:
 		return "".join(list(map(lambda char: alphabet[len(alphabet) - 1 - (alphabet.index(char))], plaintext)))
+
+
+@app.get("/vigenere-cypher/{type}")
+async def get_caesar_cypher(type, plaintext, key):
+	a, b = divmod(len(alphabet), len(key))
+	extended_key = key * a + key[:b]
+	result_string = ''
+	if type == 'encrypt':
+		for i, char in enumerate(plaintext):
+			result_string += alphabet[(alphabet.index(char) + alphabet.index(extended_key[i])) % len(alphabet)]
+	if type == 'decrypt':
+		for i, char in enumerate(plaintext):
+			result_string += alphabet[(alphabet.index(char) - alphabet.index(extended_key[i])) % len(alphabet)]
+	return result_string
+
+
